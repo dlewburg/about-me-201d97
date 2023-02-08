b539773d5386c87a023a26a25e0a24dfbb193177
Dasha Burgos <dasha.lewis@gmail.com>
2023-02-07 21:18:40 -0500

    updated styling for the website

--- js/app.js
+++ js/app.js
@@ -12,43 +12,43 @@ if (quest1 === 'yes' || quest1 === 'yeah'){
   console.log('Wow, that is CORRECT!');
   alert('Wow! That is CORRECT! You sure know your geography!');
 }else {
-  console.log('Aww, that is not correct. Let us move on to the next question.');
+  //console.log('Aww, that is not correct. Let us move on to the next question.');
   alert('Aww, that is not correct. Let us move on to the next question.');
 }
 
 let quest2 = prompt('Do I have more than one child?').toLowerCase();
 if (quest2 === 'yes' || quest2 === 'yeah'){
-  console.log('Aww, that is not correct. Let us move on to the next question.');
+  //console.log('Aww, that is not correct. Let us move on to the next question.');
   alert('Aww, that is not correct. Let us move on to the next question.');
 }else {
-  console.log('You got that one! I have a daughter.');
+  //console.log('You got that one! I have a daughter.');
   alert('You got that one right! I have a daughter.');
 }
 
 let quest3 = prompt('Was I able to finish my degree while on Active Duty in the Navy?').toLowerCase();
 if (quest3 === 'yes' || quest3 === 'yeah'){
-  console.log('Well I\'ll be, that is RIGHT!');
+  //console.log('Well I\'ll be, that is RIGHT!');
   alert('Well I\'ll be, that is RIGHT!');
 }else {
-  console.log('Hmm, that is not correct. Let us move on to the next question.');
+  //console.log('Hmm, that is not correct. Let us move on to the next question.');
   alert('Hmm, that is not correct. Let us move on to the next question.');
 }
 
 let quest4 = prompt('Did I take a Python class as my first coding class?').toLowerCase();
 if (quest4 === 'yes' || quest4 === 'yeah'){
-  console.log('Aww, that is not correct. I took a C++ class in college.');
+  //console.log('Aww, that is not correct. I took a C++ class in college.');
   alert('Aww, that is not correct. I took a C++ class in college.');
 }else {
-  console.log('Correct! I took a C++ class in college.');
+  //console.log('Correct! I took a C++ class in college.');
   alert('Correct! I took a C++ class in college.');
 }
 
 let quest5 = prompt('Do I have any pets?').toLowerCase();
 if (quest5 === 'yes' || quest5 === 'yeah'){
-  console.log('I do not, cannot, and will not have any pets. They are too messy!');
+  //console.log('I do not, cannot, and will not have any pets. They are too messy!');
   alert('I do not, cannot, and will not have any pets. They are too messy!');
 }else {
-  console.log('You hit the nail on the head! I do not, cannot, and will not have any pets. They are too messy!');
+  //console.log('You hit the nail on the head! I do not, cannot, and will not have any pets. They are too messy!');
   alert('You hit the nail on the head! I do not, cannot, and will not have any pets. They are too messy!');
 }
 
