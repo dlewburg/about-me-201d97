ab7c476bc794739fd30f9ba391f982bf0bec258f
Dasha Burgos <dasha.lewis@gmail.com>
2023-02-11 08:54:12 -0500

    removed commented out code. left comments that describe what the code does

--- js/app.js
+++ js/app.js
@@ -10,27 +10,21 @@ alert(`Hi ${visitorName}! Welcome to the page I created ALL About Me! You are go
 let scoreCount = 0;
 
 //Ask the user 5 Yes/No questions with prompt and alert for correct/incorrect answer; wrapped each question in a function
-
 function question1(){
   let quest1 = prompt('I am from Northeast Ohio, is that considered the Midwest?').toLowerCase();
   if (quest1 === 'yes' || quest1 === 'yeah'){
-    //console.log('Wow, that is CORRECT!');
     alert('Wow! That is CORRECT! You sure know your geography!');
     scoreCount++;
   }else {
-    //console.log('Aww, that is not correct. Let us move on to the next question.');
     alert('Aww, that is not correct. Let us move on to the next question.');
   }
 }
 question1();
 
 function question2(){
-  let quest2 = prompt('Do I have more than one child?').toLowerCase();
-  if (quest2 === 'yes' || quest2 === 'yeah'){
-  //console.log('Aww, that is not correct. Let us move on to the next question.');
+  let quest2 = prompt('Do I have more than one c
     alert('Aww, that is not correct. Let us move on to the next question.');
   }else {
-  //console.log('You got that one! I have a daughter.');
     alert('You got that one right! I have a daughter.');
     scoreCount++;
   }
@@ -40,11 +34,9 @@ question2();
 function question3(){
   let quest3 = prompt('Was I able to finish my degree while on Active Duty in the Navy?').toLowerCase();
   if (quest3 === 'yes' || quest3 === 'yeah'){
-    //console.log('Well I\'ll be, that is RIGHT!');
     alert('Well I\'ll be, that is RIGHT!');
     scoreCount++;
   }else {
-    //console.log('Hmm, that is not correct. Let us move on to the next question.');
     alert('Hmm, that is not correct. Let us move on to the next question.');
   }
 }
@@ -53,10 +45,8 @@ question3();
 function question4(){
   let quest4 = prompt('Did I take a Python class as my first coding class?').toLowerCase();
   if (quest4 === 'yes' || quest4 === 'yeah'){
-    //console.log('Aww, that is not correct. I took a C++ class in college.');
     alert('Aww, that is not correct. I took a C++ class in college.');
   }else {
-  //console.log('Correct! I took a C++ class in college.');
     alert('Correct! I took a C++ class in college.');
     scoreCount++;
   }
@@ -66,10 +56,8 @@ question4();
 function question5(){
   let quest5 = prompt('Do I have any pets?').toLowerCase();
   if (quest5 === 'yes' || quest5 === 'yeah'){
-    //console.log('I do not, cannot, and will not have any pets. They are too messy!');
     alert('I do not, cannot, and will not have any pets. They are too messy!');
   }else {
-  //console.log('You hit the nail on the head! I do not, cannot, and will not have any pets. They are too messy!');
     alert('You hit the nail on the head! I do not, cannot, and will not have any pets. They are too messy!');
     scoreCount++;
   }
@@ -77,6 +65,7 @@ function question5(){
 question5();
 
 function question6(){
+
 //Adding guess myNumber question with a limit of 4 Guesses
   let myNumber = 7;
   let numOfGuess = 0;
@@ -103,8 +92,8 @@ function question6(){
 question6();
 
 function question7(){
-//user guesses one element in an array within 6 tries
 
+//user guesses one element in an array within 6 tries
   let steak = ['wagyu','filet mignon','tomahawk','t-bone','kobe'];
 
   let numSteakGuess = 6;
